7a4f33b834229536616a3963e7f27e646cdea6e8
Michael Perez <mperez6473@gmail.com>
2020-11-07 14:44:10 -0600

    Spacer and WeDoMore added to HomePage

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -1,11 +1,15 @@
 import React from "react";
 import TriSection from "./TriSection";
 import HomeBanner from "./HomeBanner";
+import WeDoMore from "./WeDoMore";
+import Spacer from "./Spacer";
 export default function HomePage() {
   return (
     <>
       <HomeBanner />
       <TriSection />
+      <Spacer />
+      <WeDoMore />
     </>
   );
 }
