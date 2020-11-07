9885ec3b1436feac817bb87e7e1e6590543f5f9e
Michael Perez <mperez6473@gmail.com>
2020-11-07 14:43:09 -0600

    TriSection completed. Images for TriSection added

--- src/components/TriSection.js
+++ src/components/TriSection.js
@@ -20,10 +20,29 @@ export default function TriSection() {
         <div className="school">
           <i className="fas fa-school  fa-4x" />
           <h4>Schools</h4>
+          <p>
+            We work with schools to get their flooring needs fulfilled.
+            Pro-Select will get your logo in your lobby or any designs wanted to
+            make your building as spirited as possible. We can assist in
+            renovating your building(s) as well as assist in getting you newly
+            constructed building all ready to be walked on! We own machines to
+            make the demo work quicker. All pre-existing flooring system gets a
+            reboot by releveling and patch work where needed.
+          </p>
         </div>
-        <div className="new">
-          <i className="fas fa-city  fa-4x" />
-          <h4>All Floor Types</h4>
+        <div className="floortypes">
+          <i className="fas fa-sort-alpha-up  fa-5x" />
+          <h4>Multiple Flooring Types</h4>
+          <p>
+            Pro-Select Flooring is experienced with multiple flooring types and
+            has been actively keeping up with the latest technologies. We can
+            also go old school and work with something more classic if that is
+            what is desired. We are comfortable with all floor types from
+            Carpet, VCT, LVT, Ceramic, Sheet Vynal you name it! Rubber base
+            installed as well for floor types that require the base. We can work
+            from gym floors to sheet vynal hallways to ceramic for bathrooms.
+            Give us the challenge and we will handle it with ease.
+          </p>
         </div>
       </div>
     </section>
