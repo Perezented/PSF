f78f43f4bb75529e862da02893af1fd6e3e1c9f3
Michael Perez <mperez6473@gmail.com>
2020-11-07 14:43:50 -0600

    Experience added as well as what we do

---
+++ src/components/WeDoMore.js
@@ -0,0 +1,28 @@
+import React from "react";
+export default function WeDoMore() {
+  return (
+    <section className="weDoMore">
+      <h3>We Do More</h3>
+      <div className="gridHolder">
+        <div className="shotblast">
+          <h2>Shot Blasting</h2>
+        </div>
+        <div className="bronco">
+          <h2>Bronco</h2>
+        </div>
+        <div className="mrs">
+          <h2>Moisture Remediation Systems</h2>
+        </div>
+        <div className="leveling">
+          <h2>Self Leveling</h2>
+        </div>
+        <div className="vct">
+          <h2>VCT and LVT</h2>
+        </div>
+        <div className="ceramic">
+          <h2>Ceramic</h2>
+        </div>
+      </div>
+    </section>
+  );
+}
