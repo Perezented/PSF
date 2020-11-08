52096abcf77a68c8ce356a25b62ce3a762c5ee18
Michael Perez <mperez6473@gmail.com>
2020-11-07 20:26:12 -0600

    Addition of images for weDoMore

--- src/components/HomeBanner.js
+++ src/components/HomeBanner.js
@@ -4,7 +4,7 @@ export default function HomeBanner() {
     <section className="homeImg">
       <div className="homepageBanner">
         <h1>Pro-Select Flooring LLC.</h1>
-        <h3>29+ Years Experience</h3>
+        <h3>29+ Years of Experience</h3>
       </div>
     </section>
   );
