ded7953d8d6a531dc62f2dd1539a7bdd8e9c57e0
Michael Perez <mperez6473@gmail.com>
2020-11-08 10:29:49 -0600

    Added more imgs to the WeDoMore section

--- src/components/WeDoMore.js
+++ src/components/WeDoMore.js
@@ -2,13 +2,28 @@ import React from "react";
 export default function WeDoMore() {
   return (
     <section className="weDoMore">
-      <h3>We Do More</h3>
+      <h3>What is Pro-Select Flooring?</h3>
       <div className="gridHolder">
         <div className="shotblast">
           <h2>Shot Blasting</h2>
+          <h5>2 Machines owned, not rented.</h5>
         </div>
         <div className="bronco">
           <h2>Bronco</h2>
+          <h5>For demolition work</h5>
+        </div>
+        <div className="vynal">
+          <h2>Sheet Vynal</h2>
+        </div>
+        <div className="flashcove">
+          <h2>Flash Cove</h2>
+        </div>
+        <div className="more">
+          <h2>And Much More!</h2>
+          <h4>We Do It All!</h4>
+        </div>
+        <div className="carpet">
+          <h2>Carpet</h2>
         </div>
         <div className="mrs">
           <h2>Moisture Remediation Systems</h2>
@@ -20,7 +35,12 @@ export default function WeDoMore() {
           <h2>VCT and LVT</h2>
         </div>
         <div className="ceramic">
+          <div className="subCeramic0" />
           <h2>Ceramic</h2>
+          <div className="subCeramic1" />
+        </div>
+        <div className="nora">
+          <h2>Nora Rubber Flooring</h2>
         </div>
       </div>
     </section>
