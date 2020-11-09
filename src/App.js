489086de8bde440ee065c7df659f47a3d8fb24a8
Michael Perez <mperez6473@gmail.com>
2020-11-09 13:06:12 -0600

    Footer added w/ some styles to it to match header

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import logo from "./logo.svg";
 import "./App.scss";
 import Header from "./components/Header";
 import HomePage from "./components/HomePage";
+import Footer from "./components/Footer";
 
 function App() {
   return (
     <div className="App">
       <Header />
       <HomePage />
+      <Footer />
     </div>
   );
 }
---
+++ src/components/Footer.js
@@ -0,0 +1,15 @@
+import React from "react";
+export default function Footer() {
+  return (
+    <section className="footer">
+      <div>
+        <a href="/">Home</a>
+        <a href="/gallery">Gallery</a>
+        <a href="/about">About Us</a>
+        <a href="/contact">Contact Us</a>
+        <a href="/joinUs">Join Our Team</a>
+      </div>
+      <p>Copyright © 2020 All rights reserved</p>
+    </section>
+  );
+}
