c593c52eab89ea77e58674accdd3437f4cff8fc2
Michael Perez <mperez6473@gmail.com>
2020-11-11 18:34:21 -0600

    Contact form added to App.js

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import "./App.scss";
 import Header from "./components/Header";
 import HomePage from "./components/HomePage";
 import Footer from "./components/Footer";
+import Contact from "./components/contactUs";
 
 function App() {
   return (
     <div className="App">
       <Header />
       <HomePage />
+      <Contact />
       <Footer />
     </div>
   );
