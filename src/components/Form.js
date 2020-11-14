dc72f0ed8e8f1fc065e965e356f89328b3b50b72
Michael Perez <mperez6473@gmail.com>
2020-11-14 10:08:35 -0600

    Error fixing on classNames and htmlFors

--- src/components/Form.js
+++ src/components/Form.js
@@ -2,30 +2,30 @@ import React from "react";
 
 export default function Form(props) {
   return (
-    <div class="form">
+    <div className="form">
       <form onSubmit={props.sendEmail}>
-        <div class="fields">
-          <div class="field">
-            <label for="name">Name</label>
+        <div className="fields">
+          <div className="field">
+            <label htmlFor="name">Name</label>
             <input type="text" name="name" id="name" required />
           </div>
-          <div class="field">
-            <label for="email">Email</label>
+          <div className="field">
+            <label htmlFor="email">Email</label>
             <input type="email" name="email" id="email" required />
           </div>
-          <div class="field">
-            <label for="subject">Subject</label>
+          <div className="field">
+            <label htmlFor="subject">Subject</label>
             <input type="text" name="subject" id="subject" required />
           </div>
-          <div class="field">
-            <label class="message" for="message">
+          <div className="field">
+            <label className="message" htmlFor="message">
               Message
             </label>
             <textarea name="message" id="message" rows="4" required />
           </div>
         </div>
-        <ul class="actions">
-          <button class="join" type="submit">
+        <ul className="actions">
+          <button className="join" type="submit">
             Send Message
           </button>
         </ul>
