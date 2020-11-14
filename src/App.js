37a89863f1f17283c30227472baf1a259af79579
Michael Perez <mperez6473@gmail.com>
2020-11-13 22:39:08 -0600

    Route added to App.js

--- src/App.js
+++ src/App.js
@@ -1,16 +1,18 @@
-import logo from "./logo.svg";
 import "./App.scss";
 import Header from "./components/Header";
 import HomePage from "./components/HomePage";
 import Footer from "./components/Footer";
 import Contact from "./components/contactUs";
+import { Route, BrowserRouter as Router } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
       <Header />
-      <HomePage />
-      <Contact />
+      <Router>
+        <Route exact path="/" component={HomePage} />
+        <Route path="/contact" component={Contact} />
+      </Router>
       <Footer />
     </div>
   );
