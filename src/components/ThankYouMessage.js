f66a724af557091b71e51120d5d29f1bf1cc3936
Michael Perez <mperez6473@gmail.com>
2020-11-13 22:33:58 -0600

    Thank you message simplified. No more unused divs

--- src/components/ThankYouMessage.js
+++ src/components/ThankYouMessage.js
@@ -2,12 +2,8 @@ import React from "react";
 
 export default function ThankYouMessage(props) {
   return props.thankYouMessage === true ? (
-    <div>
-      <h4 style={{ opacity: 1 }}>Thank You, Your message has been recieved!</h4>
-    </div>
+    <h4 style={{ opacity: 1 }}>Thank You, Your message has been recieved!</h4>
   ) : (
-    <div>
-      <h4 style={{ opacity: 0 }}>You should not be able to see me</h4>
-    </div>
+    <h4 style={{ opacity: 0 }}>You should not be able to see me</h4>
   );
 }
