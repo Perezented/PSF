7c32acaa609751189f91123b12b64a4d156714ed
Michael Perez <mperez6473@gmail.com>
2020-11-20 22:54:18 -0600

    Adjusted ording of what we do

--- src/components/WeDoMore.js
+++ src/components/WeDoMore.js
@@ -4,10 +4,13 @@ export default function WeDoMore() {
     <section className="weDoMore">
       <h3>What is Pro-Select Flooring?</h3>
       <div className="gridHolder">
-        <div className="shotblast">
-          <h2>Shot Blasting</h2>
-          <h5>2 Machines owned, not rented.</h5>
-        </div>
+        {" "}
+        <div className="mrs">
+          <h2>Moisture Remediation Systems</h2>
+        </div>{" "}
+        <div className="flashcove">
+          <h2>Flash Cove</h2>
+        </div>{" "}
         <div className="bronco">
           <h2>Bronco</h2>
           <h5>For demolition work</h5>
@@ -15,32 +18,30 @@ export default function WeDoMore() {
         <div className="vynal">
           <h2>Sheet Vynal</h2>
         </div>
-        <div className="flashcove">
-          <h2>Flash Cove</h2>
-        </div>
-        <div className="more">
-          <h2>And Much More!</h2>
-          <h4>We Do It All!</h4>
-        </div>
-        <div className="carpet">
-          <h2>Carpet</h2>
-        </div>
-        <div className="mrs">
-          <h2>Moisture Remediation Systems</h2>
+        <div className="shotblast">
+          <h2>Shot Blasting</h2>
+          <h5>2 Machines owned, not rented.</h5>
         </div>
         <div className="leveling">
           <h2>Self Leveling</h2>
-        </div>
-        <div className="vct">
-          <h2>VCT and LVT</h2>
+        </div>{" "}
+        <div className="carpet">
+          <h2>Carpet</h2>
         </div>
         <div className="ceramic">
           <div className="subCeramic0" />
           <h2>Ceramic</h2>
           <div className="subCeramic1" />
         </div>
+        <div className="vct">
+          <h2>VCT and LVT</h2>
+        </div>
         <div className="nora">
           <h2>Nora Rubber Flooring</h2>
+        </div>{" "}
+        <div className="more">
+          <h2>And Much More!</h2>
+          <h4>We Do It All!</h4>
         </div>
       </div>
     </section>
