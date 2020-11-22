9ef6bc81e0b06c9238739da3f87718af9af4dd48
Michael Perez <mperez6473@gmail.com>
2020-11-21 23:44:56 -0600

    Gallery template created. Need Photo API

--- src/components/Gallery.js
+++ src/components/Gallery.js
@@ -1,8 +1,12 @@
+import img0 from "../styles/img/IMG_0331.JPG";
 export default function Gallery() {
   return (
     <section className="gallery">
       <h1>This is going to to be the gallery section</h1>
-      <div className="imgContainer"></div>
+      <div className="cardContainer">
+        <h3>Image date</h3>
+        <img src={img0} alt="image of workers on bridge" className="img0" />
+      </div>
     </section>
   );
 }
