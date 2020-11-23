bec3420516c95368ee9453eefee72d665fa1a3fa
Michael Perez <mperez6473@gmail.com>
2020-11-22 23:38:56 -0600

    Moved sendEmail out of contactUs.js into Form.js with updated form information.

--- src/components/contactUs.js
+++ src/components/contactUs.js
@@ -1,4 +1,3 @@
-import emailjs from "emailjs-com";
 import React, { useState } from "react";
 import BusinessHours from "./BusinessHours";
 import Form from "./Form";
@@ -6,45 +5,15 @@ import Banner from "./Banner";
 import ThankYouMessage from "./ThankYouMessage";
 
 export default function Contact(props) {
-  console.log(props);
   const [thankYouMessage, setThankYouMessage] = useState(false);
-  function sendEmail(e) {
-    e.preventDefault();
-    setTimeout(() => {
-      setThankYouMessage(false);
-    }, 6000);
-    setThankYouMessage(true);
-    emailjs
-      .sendForm(
-        "service_xdxjlgs",
-        "template_v0pftvj",
-        e.target,
-        "user_2WocCOhmC4a9o4yO5bwUB"
-      )
-
-      .then(
-        (result) => {
-          console.log(result.text);
-        },
-        (error) => {
-          console.log(error.text);
-        }
-      );
-    e.target.reset();
-  }
 
   return (
     <section className={props.classInfo} id={props.classInfo}>
       <Banner props={props.bannerInfo} />
-      {/* <h2>Contact Pro-Select Flooring</h2> */}
-      {/* <p className="contactMessage">
-        Let us know if there is anything we can do for you and your company. Use
-        the form below to get a hold of us directly through the email.
-      </p> */}
       <ThankYouMessage props={thankYouMessage} />
       <div className="flex">
         <BusinessHours />
-        <Form props={sendEmail} />
+        <Form props={setThankYouMessage} classInfo={props.classInfo} />
       </div>
     </section>
   );
