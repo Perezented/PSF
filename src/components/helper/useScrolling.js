efec714e9faa818f8d5bdd8f2c1136de7226065f
Michael Perez <mperez6473@gmail.com>
2020-11-28 14:19:11 -0600

    Scrolling state moved to a helper function. returns scroll data as an object when called

---
+++ src/components/helper/useScrolling.js
@@ -0,0 +1,28 @@
+import { useState, useEffect } from "react";
+
+// Returns current scroll and max window scroll. Dynamically changes with web page.
+// Returns the second index as undefined as it is the removal of the event listener
+export function useScrolling() {
+  const [windowScroll, setWindowScroll] = useState(window.scrollY);
+  const [maxWindowScroll, setMaxWindowScroll] = useState(
+    document.documentElement.scrollHeight - window.innerHeight
+  );
+  useEffect(() => {
+    window.addEventListener("scroll", handleWindowScroll);
+  }, []);
+
+  // Changes the window scroll and max window scroll
+  const handleWindowScroll = () => {
+    setWindowScroll(window.scrollY);
+    setMaxWindowScroll(
+      document.documentElement.scrollHeight - window.innerHeight
+    );
+  };
+  return {
+    curr_scroll: windowScroll,
+    max_scroll: maxWindowScroll,
+    removal_of_useEvent: () => {
+      window.removeEventListener("scroll", handleWindowScroll);
+    }
+  };
+}
