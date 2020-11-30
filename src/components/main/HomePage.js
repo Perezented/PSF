9ccaa841c11f6b396c7f57b15b13ab51e20fa634
Michael Perez <mperez6473@gmail.com>
2020-11-30 13:49:27 -0600

    Comments to homepage

--- src/components/main/HomePage.js
+++ src/components/main/HomePage.js
@@ -5,43 +5,47 @@ import WeDoMore from "../subhome/WeDoMore";
 import Spacer from "../sub/Spacer";
 import { useScrolling } from "../helper/useScrolling";
 export default function HomePage() {
+  // upon load up, classes are added using js to hide selected elements
   useEffect(() => {
     window.addEventListener("load", () => {
       document.querySelectorAll("p").forEach((value, i, array) => {
         value.classList.add("opacityBottom");
         array[0].classList.remove("opacityBottom");
       });
+
       document.querySelectorAll("section div").forEach((value, i, array) => {
         value.classList.add("opacityBottom");
         array[0].classList.remove("opacityBottom");
-        // array[1].classList.remove("opacityBottom");
         array[array.length - 1].classList.remove("opacityBottom");
-        // array[array.length - 2].classList.remove("opacityBottom");
       });
     });
   });
+
+  // Setup of specific items to have them appear once 9/10 of the screen has scrolled through
   let sections = document.querySelectorAll("section div");
   let ps = document.querySelectorAll("p");
-  let twothirds = Math.floor((window.innerHeight / 10) * 9);
+  let ninetenths = Math.floor((window.innerHeight / 10) * 9);
+  // adds 'appear' class to elements once 9/10 of the screen has passed by
   function addingAppear(array) {
     array.forEach((value, index) => {
       let currSectionTop = value.getBoundingClientRect().top;
       if (index !== array.length - 1) {
-        if (currSectionTop <= twothirds) {
+        if (currSectionTop <= ninetenths) {
           value.classList.add("appearBottom");
         }
       }
     });
   }
+
   addingAppear(sections);
   addingAppear(ps);
-
+  // props for Banner component out of homepage
   let bannerInfo = [
     "img0",
     "Pro-Select Flooring LLC.",
     "29+ Years of Experience"
   ];
-
+  // uses state to constantly check the scroll on this page
   let scrollData = useScrolling();
 
   return (
