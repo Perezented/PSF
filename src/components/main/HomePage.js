8b21e96d13d3530b926f74536f16c92aa182293e
Michael Perez <mperez6473@gmail.com>
2020-11-30 14:47:05 -0600

    Animations added to homepage

--- src/components/main/HomePage.js
+++ src/components/main/HomePage.js
@@ -7,6 +7,20 @@ import { useScrolling } from "../helper/useScrolling";
 export default function HomePage() {
   // upon load up, classes are added using js to hide selected elements
   useEffect(() => {
+    let multiDivFromSection = document.querySelectorAll("section div div");
+    let left = [
+      multiDivFromSection[3],
+      multiDivFromSection[4],
+      multiDivFromSection[8],
+      multiDivFromSection[9],
+      multiDivFromSection[13],
+      multiDivFromSection[13]
+    ];
+    let right = [
+      multiDivFromSection[6],
+      multiDivFromSection[15],
+      multiDivFromSection[15]
+    ];
     window.addEventListener("load", () => {
       document.querySelectorAll("p").forEach((value, i, array) => {
         value.classList.add("opacityBottom");
@@ -18,27 +32,48 @@ export default function HomePage() {
         array[0].classList.remove("opacityBottom");
         array[array.length - 1].classList.remove("opacityBottom");
       });
-    });
-  });
 
-  // Setup of specific items to have them appear once 9/10 of the screen has scrolled through
-  let sections = document.querySelectorAll("section div");
-  let ps = document.querySelectorAll("p");
-  let ninetenths = Math.floor((window.innerHeight / 10) * 9);
+      left.forEach((value) => {
+        value.classList.remove("opacityBottom");
+        value.classList.add("opacityLeft");
+      });
+
+      right.forEach((value) => {
+        value.classList.remove("opacityBottom");
+        value.classList.add("opacityRight");
+        console.log(value, "right");
+      });
+    });
+  }, []);
+
   // adds 'appear' class to elements once 9/10 of the screen has passed by
-  function addingAppear(array) {
+  function addingAppear(array, direction) {
     array.forEach((value, index) => {
       let currSectionTop = value.getBoundingClientRect().top;
-      if (index !== array.length - 1) {
+      if (index !== array.length) {
         if (currSectionTop <= ninetenths) {
-          value.classList.add("appearBottom");
+          value.classList.add(direction);
         }
       }
     });
   }
-
-  addingAppear(sections);
-  addingAppear(ps);
+  // Setup of specific items to have them appear once 9/10 of the screen has scrolled through
+  let sections = document.querySelectorAll("section div");
+  let ps = document.querySelectorAll("p");
+  let ninetenths = Math.floor((window.innerHeight / 10) * 9);
+  let multiDivFromSection = document.querySelectorAll("section div div");
+  let left = [
+    multiDivFromSection[3],
+    multiDivFromSection[4],
+    multiDivFromSection[8],
+    multiDivFromSection[9],
+    multiDivFromSection[13]
+  ];
+  let right = [multiDivFromSection[6], multiDivFromSection[15]];
+  addingAppear(sections, "appearBottom");
+  addingAppear(ps, "appearBottom");
+  left[0] && addingAppear(left, "appearLeft");
+  left[0] && addingAppear(right, "appearRight");
   // props for Banner component out of homepage
   let bannerInfo = [
     "img0",
