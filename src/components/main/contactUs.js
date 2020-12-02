a194d12019354f3d642fc3fd10b42125f8bd0f0a
Michael Perez <mperez6473@gmail.com>
2020-12-02 17:33:19 -0600

    contact us page displays all elements. All have opacity of 1 now

--- src/components/main/contactUs.js
+++ src/components/main/contactUs.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import BusinessHours from "../sub/BusinessHours";
 import Form from "../sub/Form";
 import Banner from "../sub/Banner";
@@ -6,7 +6,14 @@ import ThankYouMessage from "../sub/ThankYouMessage";
 
 export default function Contact(props) {
   const [thankYouMessage, setThankYouMessage] = useState(false);
-
+  useEffect(() => {
+    document.querySelectorAll("section div").forEach((value) => {
+      value.classList.remove("opacityBottom");
+    });
+    document.querySelectorAll("p").forEach((value) => {
+      value.classList.remove("opacityBottom");
+    });
+  }, []);
   return (
     <section className={props.classInfo + " fade"} id={props.classInfo}>
       <Banner props={props.bannerInfo} />
