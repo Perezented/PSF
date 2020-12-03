14ff6bbf0366689a26d811117ba03dca378ae03b
Michael Perez <mperez6473@gmail.com>
2020-12-03 11:51:53 -0600

    Thank you message reduced to just a single h4

--- src/components/sub/ThankYouMessage.js
+++ src/components/sub/ThankYouMessage.js
@@ -1,9 +1,7 @@
 import React from "react";
 
 export default function ThankYouMessage(props) {
-  return props.thankYouMessage === true ? (
-    <h4 style={{ opacity: 1 }}>Thank You, Your message has been recieved!</h4>
-  ) : (
-    <h4 style={{ opacity: 0 }}>You should not be able to see me</h4>
+  return (
+    <h4 className="tymessage">Thank You, Your message has been recieved!</h4>
   );
 }
