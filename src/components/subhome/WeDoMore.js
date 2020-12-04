cd19d1c7d14db3495ad3218ec4d4bc2d139786b9
Michael Perez <mperez6473@gmail.com>
2020-12-03 18:00:53 -0600

    Removal of unwanted code.

--- src/components/subhome/WeDoMore.js
+++ src/components/subhome/WeDoMore.js
@@ -1,16 +1,14 @@
-import React from "react";
 export default function WeDoMore() {
   return (
     <section className="weDoMore">
       <h3>What is Pro-Select Flooring?</h3>
       <div className="gridHolder">
-        {" "}
         <div className="mrs">
           <h2>Moisture Remediation Systems</h2>
-        </div>{" "}
+        </div>
         <div className="flashcove">
           <h2>Flash Cove</h2>
-        </div>{" "}
+        </div>
         <div className="bronco">
           <h2>Bronco</h2>
           <h5>For demolition work</h5>
@@ -24,7 +22,7 @@ export default function WeDoMore() {
         </div>
         <div className="leveling">
           <h2>Self Leveling</h2>
-        </div>{" "}
+        </div>
         <div className="carpet">
           <h2>Carpet</h2>
         </div>
@@ -38,7 +36,7 @@ export default function WeDoMore() {
         </div>
         <div className="nora">
           <h2>Nora Rubber Flooring</h2>
-        </div>{" "}
+        </div>
         <div className="more">
           <h2>And Much More!</h2>
           <h4>We Do It All!</h4>
