96e5a89a979ab2670bb7683077777cbb5e826a55
Michael Perez <mperez6473@gmail.com>
2020-12-06 13:26:52 -0600

    More typo fixes in TriSection

--- src/components/subhome/TriSection.js
+++ src/components/subhome/TriSection.js
@@ -12,25 +12,26 @@ export default function TriSection() {
             Our team is accepted by multiple hospitals ranging from the states
             of Utah to Arkansas. Pro-Select Flooring understands a hospital
             environment has the requirement to be clean and contain fresh air,
-            so we ensure to keep dust and contaminats at a minimun. We have
+            so we ensure to keep dust and contaminants at a minimum. We have
             focused most of our operations in the Tulsa, OK and Oklahoma City
             areas, with nearly all major hospitals in these areas hiring
-            Pro-Select to get all flooring needs fulfilled.{" "}
+            Pro-Select to get all flooring needs fulfilled.
           </p>
         </div>
         <div className="school">
           <i className="fas fa-school  fa-4x" />
           <h4>Schools, Churches, Businesses</h4>
           <p>
-            We work with schools, churches and all different kinds of businesses
-            to get their flooring needs fulfilled. Pro-Select is capable of
-            getting your school or business logo on your lobby floor and/or any
-            designs throughout the building wanted to make your building as
-            spirited as possible. We can assist in renovating your building(s)
-            as well as assist in getting your newly constructed building all
-            ready to be walked on! We own machines to make the demo work quicker
-            and have all pre-existing flooring systems get a reboot by
-            releveling and patch work where needed.
+            We work with schools, churches, and all different kinds of
+            businesses to get their flooring needs fulfilled. Pro-Select is
+            capable of getting your school or business logo on your lobby floor
+            and/or any designs throughout the building wanted to make your
+            building as spirited as possible. We can assist in renovating your
+            building(s) as well as assist in getting your newly constructed
+            building all ready to be walked on for multiple years to come! We
+            own machines to make the demo work quicker and have all pre-existing
+            flooring systems get a reboot by releveling and patch work where
+            needed.
           </p>
         </div>
         <div className="floortypes">
@@ -44,9 +45,9 @@ export default function TriSection() {
             Carpet, VCT, LVT, Ceramic, Sheet Vinyl, you name it, we do it!
             Rubber base is installed as well for floor types that require the
             base. You do not get half jobs marked as completed, you get the full
-            package. We can work on gym floors, to sheet vinyl hallways, to
-            ceramic for bathrooms. Give us the challenge and we will handle it
-            with ease.
+            package. We can work on gym floors, sheet vinyl hallways, to ceramic
+            for bathrooms. Give us the challenge and we will handle it with
+            ease.
           </p>
         </div>
       </div>
